5d603ef6884443c7ea140e9beeb67aa25972bb18
zhengguang <zg.zhu@daocloud.io>
2017-11-19 01:33:34 +0800

    Update play-with-k8s.com.sh

--- play-with-k8s.com.sh
+++ play-with-k8s.com.sh
@@ -2,6 +2,7 @@
 #use: curl https://raw.githubusercontent.com/zgfh/test/master/play-with-k8s.com.sh|sh - ngrok_token 
 #I want a shell for test
 ngrok_token=$1
+rm -rf ngrok.zip ngrok
 yum -y install -qqy openssh-server openssh-clients unzip wget curl htop
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -O ngrok.zip
 unzip ngrok.zip
